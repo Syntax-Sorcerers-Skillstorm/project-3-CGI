e636befc1024739ed737528025e01bdffdaa8d1c
stephen-c-perry <stephen.christian.perry@gmail.com>
2024-05-22 22:10:31 -0500

    removed creds until i can mask

--- quiz-app/src/main/java/com/skillstorm/quizapp/models/Questions.java
+++ quiz-app/src/main/java/com/skillstorm/quizapp/models/Questions.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Entity
 @Data
 public class Questions {
+    @ManyToOne
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long questionId;
